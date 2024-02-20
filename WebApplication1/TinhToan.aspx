6f9aafc934cbb44243af53806e80c4bf21777f59
ACER <ACER@LAPTOP-QE4L2J6B>
2024-02-20 15:33:30 +0700

    Add project files.

---
+++ WebApplication1/TinhToan.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="TinhToan.aspx.cs" Inherits="WebApplication1.TinhToan" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            tính toán đơn giản
+            <br/>
+            Nhập số 1<asp:TextBox ID="txtso1" runat="server"></asp:TextBox>
+            <br/>
+            Nhập Số 2<asp:TextBox ID="txtso2" runat="server"></asp:TextBox>
+            <br />
+            <asp:Button ID="Button1" runat="server" Text="cộng" OnClick="Button1_Click" />
+            <asp:Button ID="Button2" runat="server" Text="trừ" OnClick="Button2_Click" />
+            <asp:Button ID="Button3" runat="server" Text="nhân" Width="42px" OnClick="Button3_Click" />
+            <asp:Button ID="Button4" runat="server" Text="chia" OnClick="Button4_Click" />
+            <br />
+            Kết quả <asp:TextBox ID="txtkq" runat="server"></asp:TextBox>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/TinhToan.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class TinhToan : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            double s1 = Double.Parse(txtso1.Text);
+            double s2 = Double.Parse(txtso2.Text);
+
+            double cong = s1 + s2;
+            txtkq.Text = cong.ToString();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            double s1 = Double.Parse(txtso1.Text);
+            double s2 = Double.Parse(txtso2.Text);
+
+            double tru = s1 - s2;
+            txtkq.Text = tru.ToString();
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            double s1 = Double.Parse(txtso1.Text);
+            double s2 = Double.Parse(txtso2.Text);
+
+            double nhan = s1 * s2;
+            txtkq.Text = nhan.ToString();
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            double s1 = Double.Parse(txtso1.Text);
+            double s2 = Double.Parse(txtso2.Text);
+
+            double chia = s1 / s2;
+            txtkq.Text = chia.ToString();
+        }
+    }
+}
